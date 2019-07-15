40e3d0cecf63cc7947a235dc2a1efe24c4fd46b1
Kartik Madhira <kartikmadhira1@gmail.com>
2019-07-14 23:35:49 -0700

    cleaned repo

--- CppND-System-Monitor/Process.h
+++ CppND-System-Monitor/Process.h
@@ -45,6 +45,7 @@ string Process::getProcess(){
     }
 
     return (this->pid + "   " + 
+            this->user + "   " +
             this->mem.substr(0,5) + "   " + 
             this->cpu.substr(0,5)  + "   " + 
             this->upTime.substr(0,5)  + "   " + 
