1f096d9c67970ad3622a935144ad4fcba3a2dea6
Kartik Madhira <kartikmadhira1@gmail.com>
2019-07-14 20:02:04 -0700

    final project changes

--- CppND-Route-Planning-Project/src/main.cpp
+++ CppND-Route-Planning-Project/src/main.cpp
@@ -53,13 +53,19 @@ int main(int argc, const char **argv)
     // TODO: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below.
-
+	float start_x, start_y, end_x, end_y;
     // Build Model.
     RouteModel model{osm_data};
+	std::cin >> start_x;
+  	std::cin >> start_y ;
 
+  	std::cin >> end_x ;
+	std::cin >> end_y ;
     // Perform search and render results.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
+    route_planner.AStarSearch();
     Render render{model};
+    std::cout<< route_planner.GetDistance() << '\n';
 
     auto display = io2d::output_surface{400, 400, io2d::format::argb32, io2d::scaling::none, io2d::refresh_style::fixed, 30};
     display.size_change_callback([](io2d::output_surface& surface){
--- CppND-Route-Planning-Project/src/route_planner.h
+++ CppND-Route-Planning-Project/src/route_planner.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef ROUTE_PLANNER_H
+#define ROUTE_PLANNER_H
 
 #include <iostream>
 #include <vector>
@@ -6,12 +7,29 @@
 #include "route_model.h"
 
 
+
 class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
 
+    const float GetDistance(){
+      return distance;
+    }
+    void AStarSearch();
+
   private:
     // Add private variables or methods declarations here.
     RouteModel &m_Model;
+    RouteModel::Node *start_node, *end_node;
+    float distance;
+    std::vector<RouteModel::Node> ConstructFinalPath(RouteModel::Node *current_node);
+    float CalculateHValue(const RouteModel::Node* );
+    std::vector<RouteModel::Node*> open_list;
+    RouteModel::Node * NextNode();
+    void AddNeighbors(RouteModel::Node *node);
 };
+
+
+
+#endif
